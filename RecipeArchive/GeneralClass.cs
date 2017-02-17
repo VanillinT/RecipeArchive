b6c5c6a2b9d0978e18c55753d33a1a57264dbd52
Ivan Tominets <ivtominets@edu.hse.ru>
2017-02-18 02:14:00 +0300

    Created a general class and some inner classes with basic fields, started creating an interface.

---
+++ RecipeArchive/GeneralClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeArchive
+{
+    class GeneralClass
+    {
+        private string _kind;
+
+        public string Kind
+        {
+            get { return _kind; }
+            set { _kind = value; }
+        }
+
+        class CookBook : GeneralClass
+        {
+            private string _nameofdish;
+
+            public string NameOfDish
+            {
+                get { return _nameofdish; }
+                set { _nameofdish = value; }
+            }
+
+            private string _recipe;
+
+            public string Recipe
+            {
+                get { return _recipe; }
+                set { _recipe = value; }
+            }
+
+            private int _timeofcookong;
+
+            public int TimeOfCooking
+            {
+                get { return _timeofcookong; }
+                set { _timeofcookong = value; }
+            }
+        }
+
+        class Games : GeneralClass
+        {
+            private string _name;
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            class Minecraft : Games
+            {
+                private string _resourses;
+
+                public string Resourses
+                {
+                    get { return _resourses; }
+                    set { _resourses = value; }
+                }
+
+                private string _isused;
+
+                public string IsUsed
+                {
+                    get { return _isused; }
+                    set { _isused = value; }
+                }
+
+            }
+        }
+    }
+}
